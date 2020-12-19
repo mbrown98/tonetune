743aa6c7e823c2b8f849b6d9a2bc3204a82152a5
Matt Brown <matthewbrown0798@gmail.com>
2020-12-19 13:32:38 -0500

    added lyric page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/layout/NavBar";
 import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
 import { Provider } from "./context";
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
+              <Route eact path="/lyrics/track/:id" component={Lyrics} />
             </Switch>
           </div>
         </React.Fragment>
